9e59a416cf010c00d6da42b04feb902d4a099897
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-17 23:19:10 +0300

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task №1:
+// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+// Так как в недели 7 дней, то вероятные значения чисел от 1 до 7 включительно. 
+
+Console.WriteLine("Введие число от 1 до 7 включительно");
+int A = int.Parse(Console.ReadLine());
+
+ if (A == 1) Console.WriteLine("Это будний день "); 
+ if (A == 2) Console.WriteLine("Это будний день "); 
+ if (A == 3) Console.WriteLine("Это будний день "); 
+ if (A == 4) Console.WriteLine("Это будний день "); 
+ if (A == 5) Console.WriteLine("Это будний день "); 
+ 
+if (A == 6) Console.WriteLine("Это ВЫХОДНОЙ!!! "); 
+if (A == 7) Console.WriteLine("Это ВЫХОДНОЙ!!! "); 
+
+if (A < 1) Console.WriteLine("Вы ввели число меньше 1, пожалуйста введите число от 1 до 7");
+if (A > 7) Console.WriteLine("Вы ввели число больше 7, пожалуйста введите число от 1 до 7");
+
