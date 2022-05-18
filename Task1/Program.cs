7aab2a02d91da796ccb705816a4d5df4c4d352d3
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-18 13:13:54 +0300

    Make change in Task3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,15 +6,17 @@
 Console.WriteLine("Введие число от 1 до 7 включительно");
 int A = int.Parse(Console.ReadLine());
 
- if (A == 1) Console.WriteLine("Это будний день "); 
- if (A == 2) Console.WriteLine("Это будний день "); 
- if (A == 3) Console.WriteLine("Это будний день "); 
- if (A == 4) Console.WriteLine("Это будний день "); 
- if (A == 5) Console.WriteLine("Это будний день "); 
- 
+ // if (A == 1) Console.WriteLine("Это будний день "); 
+ // if (A == 2) Console.WriteLine("Это будний день "); 
+ // if (A == 3) Console.WriteLine("Это будний день "); 
+ // if (A == 4) Console.WriteLine("Это будний день "); 
+ // if (A == 5) Console.WriteLine("Это будний день "); 
+
+
 if (A == 6) Console.WriteLine("Это ВЫХОДНОЙ!!! "); 
-if (A == 7) Console.WriteLine("Это ВЫХОДНОЙ!!! "); 
+else if (A == 7) Console.WriteLine("Это ВЫХОДНОЙ!!! "); 
 
-if (A < 1) Console.WriteLine("Вы ввели число меньше 1, пожалуйста введите число от 1 до 7");
-if (A > 7) Console.WriteLine("Вы ввели число больше 7, пожалуйста введите число от 1 до 7");
+else if (A < 1) Console.WriteLine("Вы ввели число меньше 1, пожалуйста введите число от 1 до 7");
+else if (A > 7) Console.WriteLine("Вы ввели число больше 7, пожалуйста введите число от 1 до 7");
 
+else Console.WriteLine("Это будний день");
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,6 +1,16 @@
 ﻿// Task №3
 // Задать номер четверти, показать диапазоны для возможных координат
 
+Console.WriteLine("Укажите номер четверти (от 1 до 4) и мы увидим возможный диапозон значений для X и Y ");
 
+int A = int.Parse(Console.ReadLine());
+
+if (A == 1) Console.WriteLine("Для первой четверти Х > 0; Y > 0. ");
+else if (A == 2) Console.WriteLine("Для второй четверти X < 0; Y > 0");
+else if (A == 3) Console.WriteLine("Для третьей четверти X < 0; Y < 0"); 
+else if (A ==4) Console.WriteLine("Для четвертой четверти X > 0; Y < 0");
+
+else if (A > 4) Console.WriteLine("Вы ввели число больше заданого диапозона, повторите попытку. ");
+else if (A < 1) Console.WriteLine("Вы ввели число меньше заданного диапозона, повторите попытку"); 
 
 
